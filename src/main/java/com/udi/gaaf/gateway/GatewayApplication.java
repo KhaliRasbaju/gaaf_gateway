1bb6503182529673b1933840e719056d08cdce64
KhaliRasbaju <ksalamanca1@udi.edu.co>
2025-10-08 17:25:01 -0500

    Cambios en la configuracion

--- src/main/java/com/udi/gaaf/gateway/GatewayApplication.java
+++ src/main/java/com/udi/gaaf/gateway/GatewayApplication.java
@@ -2,7 +2,10 @@ package com.udi.gaaf.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 	public static void main(String[] args) {
